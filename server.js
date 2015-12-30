6338d712554256120b9236a1e1e91a69077e002a
Elana Kopelevich <ekopelevich@gmail.com>
2015-12-30 14:30:21 -0700

    initial commit

--- server.js
+++ server.js
@@ -2,4 +2,26 @@
 // that will send back the index.html file on a GET request to '/'
 // it should then send back jsonData on a GET to /data
 
+var express = require('express');
+var app = express();
+var fs = require('fs');
+
+app.use(express.static('public'));
+
 var jsonData = {count: 12, message: 'hey'};
+
+app.get('/', function(req, res){
+  var index = fs.readFile('../index', function (err, data) {
+    if (err) throw err;
+  });
+  res.send(index);
+
+});
+
+app.get('/data', function(req, res){
+  res.json(jsonData);
+});
+
+app.listen(3000, function(){
+  console.log('Listening on port 3000');
+});
