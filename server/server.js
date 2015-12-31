50987f5c34000989732d820b51a83d483fb5241b
Elana Kopelevich <ekopelevich@gmail.com>
2015-12-31 13:33:37 -0700

    completed step 2

--- server/server.js
+++ server/server.js
@@ -23,5 +23,80 @@ var id = 0;
 
 // TODO: make the REST routes to perform CRUD on lions
 
+app.get('/lions', function(req, res){
+  res.json(lions);
+});
+
+app.get('/lions/:id', function(req, res){
+  var lion = _.find(lions, {id: req.params.id});
+  res.json(lion || 'There are no lions here.');
+});
+
+app.post('/lions', function(req, res){
+  var lion = req.body;
+  id++;
+  lion.id = id + '';
+  lions.push(lion);
+  res.json(lion);
+});
+
+app.put('/lions/:id', function(req, res){
+  var update = req.body;
+  if (update.id) {
+    delete update.id;
+  }
+
+  var lion = _.findIndex(lions, {id: req.params.id});
+  if(!lions[lion]){
+    req.send('That lion does no exist.');
+  } else {
+    var updatedLion = _.assign(lions[lion], update);
+    res.json(updatedLion);
+  }
+});
+
+app.delete('/lions/:id', function(req, res){
+  var lion = _.findIndex(lions, {id: req.params.id});
+  if(!lions[lion]){
+    req.send('That lion does no exist.');
+  } else {
+    var deletedLion = lions[lion];
+    lions.splice(deletedLion, 1);
+    res.json(deletedLion);
+  }
+});
+
+// {
+//   "GET /lions": {
+//     "desc": "returns all lions",
+//     "response": "200 application/json",
+//     "data": [{}, {}, {}]
+//   },
+//
+//   "GET /lions/:id": {
+//     "desc": "returns one lion respresented by its id",
+//     "response": "200 application/json",
+//     "data": {}
+//   },
+//
+//   "POST /lions": {
+//     "desc": "create and returns a new lion uisng the posted object as the lion",
+//     "response": "201 application/json",
+//     "data": {}
+//   },
+//
+//   "PUT /lions/:id": {
+//     "desc": "updates and returns the matching lion with the posted update object",
+//     "response": "200 application/json",
+//     "data": {}
+//   },
+//
+//   "DELETE /lions/:id": {
+//     "desc": "deletes and returns the matching lion",
+//     "response": "200 application/json",
+//     "data": {}
+//   }
+// }
+
 app.listen(3000);
 console.log('on port 3000');
